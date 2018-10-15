ea12a01ef58044c4512d22eb7c13fea12c54df79
Maniesha Abeysekera <mabeysekera@CMDMABEYSEKERA.local>
2018-10-15 15:27:29 +0530

    First commit

---
+++ SuaveASample/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SuaveASample
+{
+    public partial class MainPage : ContentPage
+    {
+        private TapGestureRecognizer tg;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            answerStack.IsVisible = false;
+
+            tg = new TapGestureRecognizer();
+            tg.Tapped += Tg_Tapped;
+            questionStack.GestureRecognizers.Add(tg);
+        }
+
+        public Animation animate;
+
+        void Tg_Tapped(object sender, EventArgs e)
+        {
+            var answerHeight = answerStack.Height;
+
+            answerStack.IsVisible = !answerStack.IsVisible;
+            questionStack.GestureRecognizers.Remove(tg);
+
+            animate = new Animation(d => answerStack.HeightRequest = d, 0, answerHeight);
+
+            animate.Commit(answerStack, "a", 5, 350, Easing.CubicIn, (data, x) => {
+                Device.BeginInvokeOnMainThread(() => {
+                    answerStack.HeightRequest = answerHeight;
+                    questionStack.GestureRecognizers.Add(tg);
+                });
+            }, null);
+        }
+    }
+}
